93d7b28155ab0308be4e1cb60d5bc3b1e96d5468
Nguyen Hong Duc <37604401+NguyenHongDuc2307@users.noreply.github.com>
2021-09-20 19:37:34 +0900

    Add files via upload

--- edge_detect_2.py
+++ edge_detect_2.py
@@ -16,17 +16,12 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray_g = (image[:,:,0]+ gray)/2
 gray_g=cv2.convertScaleAbs(gray_g)
 
-#gray = image[:,:,0]
-#blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-blurred = gray_g
-# show the original and blurred images
+
 cv2.imshow("Original", image)
-cv2.imshow("Blurred", blurred)
-wide = cv2.Canny(blurred, 100, 180)
-#wide = cv2.Canny(blurred, 180, 200)
-mid = cv2.Canny(blurred, 30, 150)
-#mid = cv2.Canny(blurred, 100, 120)
-tight = cv2.Canny(blurred, 340, 350)
+cv2.imshow("Gray scale", gray_g)
+wide = cv2.Canny(gray_g, 100, 180)
+mid = cv2.Canny(gray_g, 30, 150)
+tight = cv2.Canny(gray_g, 340, 350)
 
 print(tight)
 # show the output Canny edge maps
